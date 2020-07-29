4910a35c05e59036252407c5f5121abdc6880143
theone <xuguangyue@dtrobo.com>
2020-07-29 11:46:33 +0800

    flutter navigation param study

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,69 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      title: 'flutter acigation study',
-      home: new FirstScreen(),
-    ),
-  );
+void main() => {
+      runApp(
+        MaterialApp(
+          title: '导航参数的传递和接收',
+          home: ProductList(
+            products: List.generate(
+              10,
+              (i) => Product('商品 $i', '这是一个商品详情，编号为:$i'),
+            ),
+          ),
+        ),
+      )
+    };
+
+class Product {
+  final String title; // 商品标题
+  final String description; // 商品描述
+  Product(this.title, this.description);
 }
 
-class FirstScreen extends StatelessWidget {
+class ProductList extends StatelessWidget {
+  final List<Product> products;
+  ProductList({Key key, @required this.products}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('flutter acigation study'),
+        title: Text('商品列表'),
       ),
-      body: Center(
-        child: RaisedButton(
-            child: Text('前往下一页'),
-            onPressed: () {
-              Navigator.push(
+      body: ListView.builder(
+          itemCount: products.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(products[index].title),
+              onTap: () => {
+                Navigator.push(
                   context,
-                  new MaterialPageRoute(
-                    builder: (context) => new SecondScreen(),
-                  ));
-            }),
-      ),
+                  MaterialPageRoute(
+                    builder: (context) => new ProductDetails(
+                      product: products[index],
+                    ),
+                  ),
+                ),
+              },
+            );
+          }),
     );
   }
 }
 
-class SecondScreen extends StatelessWidget {
+class ProductDetails extends StatelessWidget {
+  // 接受参数
+  final Product product;
+  ProductDetails({Key key, @required this.product}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('子页面'),
+        title: Text(
+          '${product.title}',
+        ),
       ),
       body: Center(
-        child: RaisedButton(
-          child: Text('返回'),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
+        child: Text('${product.description}'),
       ),
     );
   }
